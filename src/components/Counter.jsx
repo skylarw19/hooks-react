f4b0116725d3cac4bf4ea66c6d7a0eef268f3016
Skylar Wong <li.q.wong@gmail.com>
2021-01-12 16:30:05 -0600

    add explanatory comment to useRef Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,7 +4,11 @@ const Counter = () => {
 
     const counter = useRef(0);
 
+    // if we do just counter.current = counter.current + 1 outside of the useEffect,
+    // then the ref value would be updated during the render phase, which means the value can be incremented more than once.
+    // therefore, refs should be modified in event handlers or in effects, not in render phase
     useEffect(() => {
+        // every time the component has been re-rendered, the counter is incremented
         counter.current = counter.current + 1;
     });
 
